e473fcd072574cb2a3731577d3a6b03096f84cc9
silentpaw <145987682+silentpaw@users.noreply.github.com>
2025-01-24 13:22:24 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -11,6 +11,6 @@ def test_magazine(browser):
      link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
      browser.get(link)
      button = WebDriverWait(browser, 100).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'button[value="Добавить в корзину"]'))
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'button[type="submit"]'))
         )
     
